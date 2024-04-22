4d315b9786e068237d0d7c87aa6c1fb6b4032c84
num13 <num13-ne@mail.ru>
2024-04-22 21:00:30 +0300

    Test and code

--- src/main/java/TicketTimeComparator.java
+++ src/main/java/TicketTimeComparator.java
@@ -1,2 +1,19 @@
-package PACKAGE_NAME;public class TicketTimeComparator {
+import java.util.Comparator;
+
+public class TicketTimeComparator implements Comparator<Ticket> {
+
+    @Override
+    public int compare(Ticket t1, Ticket t2) {
+        // ваш код
+        int flyTime1 = t1.getTimeTo() - t1.getTimeFrom();
+        int flyTime2 = t2.getTimeTo() - t2.getTimeFrom();
+
+        if (flyTime1 > flyTime2) {
+            return 1;
+        } else if (flyTime1 < flyTime2) {
+            return -1;
+        } else {
+            return 0;
+        }
+    }
 }
--- src/test/java/AviaSoulsTest.java
+++ src/test/java/AviaSoulsTest.java
@@ -1,2 +1,87 @@
-package PACKAGE_NAME;public class AviaSoulsTest {
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class AviaSoulsTest {
+
+    @Test
+    public void testSortCompare() {
+        AviaSouls aviaSouls = new AviaSouls();
+        Ticket ticket1 = new Ticket("msk", "spb", 100, 3, 12); //2
+        Ticket ticket2 = new Ticket("spb", "msk", 200, 4, 6);
+        Ticket ticket3 = new Ticket("msk", "spb", 700, 9, 15); //3
+        Ticket ticket4 = new Ticket("msk", "kz", 300, 18, 23);
+        Ticket ticket5 = new Ticket("msk", "spb", 50, 1, 3); //1
+
+        aviaSouls.add(ticket1);
+        aviaSouls.add(ticket2);
+        aviaSouls.add(ticket3);
+        aviaSouls.add(ticket4);
+        aviaSouls.add(ticket5);
+
+        Ticket[] expected = {ticket5, ticket1, ticket3};
+        Assertions.assertArrayEquals(aviaSouls.search("msk", "spb"), expected);
+    }
+
+    @Test
+    public void testSortCompareEmpty() {
+        AviaSouls aviaSouls = new AviaSouls();
+        Ticket ticket1 = new Ticket("msk", "spb", 100, 3, 12);
+        Ticket ticket2 = new Ticket("spb", "msk", 200, 4, 6);
+        Ticket ticket3 = new Ticket("msk", "spb", 700, 9, 15);
+        Ticket ticket4 = new Ticket("msk", "kz", 300, 18, 23);
+        Ticket ticket5 = new Ticket("msk", "spb", 50, 1, 3);
+
+        aviaSouls.add(ticket1);
+        aviaSouls.add(ticket2);
+        aviaSouls.add(ticket3);
+        aviaSouls.add(ticket4);
+        aviaSouls.add(ticket5);
+
+        Ticket[] expected = {};
+        Assertions.assertArrayEquals(aviaSouls.search("kz", "spb"), expected);
+    }
+
+    @Test
+    public void testComparable() {
+        AviaSouls aviaSouls = new AviaSouls();
+        Ticket ticket1 = new Ticket("msk", "spb", 100, 9, 15); //2
+        Ticket ticket2 = new Ticket("spb", "msk", 200, 4, 6);
+        Ticket ticket3 = new Ticket("msk", "spb", 700, 3, 12); //3
+        Ticket ticket4 = new Ticket("msk", "kz", 300, 18, 23);
+        Ticket ticket5 = new Ticket("msk", "spb", 50, 1, 3); //1
+
+        aviaSouls.add(ticket1);
+        aviaSouls.add(ticket2);
+        aviaSouls.add(ticket3);
+        aviaSouls.add(ticket4);
+        aviaSouls.add(ticket5);
+
+        TicketTimeComparator comparator = new TicketTimeComparator();
+
+        Ticket[] expected = {ticket5, ticket1, ticket3};
+        Assertions.assertArrayEquals(aviaSouls.searchAndSortBy("msk", "spb", comparator), expected);
+
+    }
+
+    @Test
+    public void testComparableEmpty() {
+        AviaSouls aviaSouls = new AviaSouls();
+        Ticket ticket1 = new Ticket("msk", "spb", 100, 9, 15); //2
+        Ticket ticket2 = new Ticket("spb", "msk", 200, 4, 6);
+        Ticket ticket3 = new Ticket("msk", "spb", 700, 3, 12); //3
+        Ticket ticket4 = new Ticket("msk", "kz", 300, 18, 23);
+        Ticket ticket5 = new Ticket("msk", "spb", 50, 1, 3); //1
+
+        aviaSouls.add(ticket1);
+        aviaSouls.add(ticket2);
+        aviaSouls.add(ticket3);
+        aviaSouls.add(ticket4);
+        aviaSouls.add(ticket5);
+
+        TicketTimeComparator comparator = new TicketTimeComparator();
+
+        Ticket[] expected = {};
+        Assertions.assertArrayEquals(aviaSouls.searchAndSortBy("kz", "spb", comparator), expected);
+
+    }
 }
